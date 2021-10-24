77efb350bdd665320af90bb1478e7eab3ad46ede
Bruno Caneo <brunocaneo3@gmail.com>
2021-10-24 14:20:49 -0300

    Fixing bugs

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import illustrationImg from '../assets/illustration.svg';
+
+export function Home(){
+  return(
+    <div>
+      <aside>
+        <img src={illustrationImg} alt="Illustration" />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div>
+          
+        </div>
+      </main>
+    </div>
+  )
+}
